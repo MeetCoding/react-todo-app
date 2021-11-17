55d64941aacb201d813c6367f7a46b98fd839e61
MeetCoding <meet.prakash.shah@gmail.com>
2021-11-17 10:25:06 +0530

    added color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-purple': '#3E3673',
+        'purple': '#ABA0F2',
+        'yellow': '#F2E527',
+        'white': '#F2F2F0',
+        'dark-yellow': '#F2C230',
+      }
+    },
   },
   variants: {
     extend: {},
